ff0593c1d3a1498b049a41b81c0bf8f2a5df9a13
Leo <73035869+leo307@users.noreply.github.com>
2023-03-28 22:35:49 -0400

    Update tokyo_remaster_r.js

--- tokyo_remaster_r.js
+++ tokyo_remaster_r.js
@@ -64,18 +64,7 @@
     Cheat.PrintColor(logo_clr, "░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░" + "\n");
     Cheat.PrintChat("Initializing Tokyo for ID: " + Entity.GetSteamID(Entity.GetLocalPlayer()) + ", User: " + Cheat.GetUsername() + "\n");
     /*
-    ⢀⠤⡀
- ⢠⠊⣉⠒⠤⢀⡀          ⡐⢁⠴⢜⢄
- ⡎⢸  ⠉⠐⠢⢌⠑⢄    ⡸  ⡆    ⠣⠱⡀
- ⡇⢸        ⣀⠗  ⠉⠉⠁  ⠙⠢⠤⡀⢃⢱
- ⡇⠘⣄⢀⠔⠉                    ⠈⠁⠘⡄
- ⢇    ⠁                        ⠘⡄
- ⢸            ⢀⣀⣀⡀        ⢀⣀⣀⡀  ⢣
- ⡸        ⢴⣾⡿⠿⠽⠇        ⠘⠛⠛⠛  ⠈⢄
-⠰⡁              ⢠⠒⠢⡀.⠒.      ⡠⢄  ⡘
- ⠱⣀          ⢀⠜    ⠇        ⢀⠔⠁  ⡏
-     ⠑⠤⢄⣀⠔⠁    ⡜        ⠊⠁  ⢀⠜
-*/
+
 
     // set name selection back to first option 
     UI.SetValue(["Misc.", "Tokyo Whitelist", "Tokyo Whitelist", "Name Selection"], 0);
